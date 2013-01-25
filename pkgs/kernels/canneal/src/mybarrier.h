4ad8475a0119a2f94acfb6a485f53c7ab17b5c6c
Abhisek Pan <pana@sherpa21.ecn.purdue.edu>
2013-01-25 18:16:39 -0500

    changed the custom barrier code

--- pkgs/kernels/canneal/src/mybarrier.h
+++ pkgs/kernels/canneal/src/mybarrier.h
@@ -17,30 +17,37 @@ class MyBarrier {
   
  private:
   int m_participants;
-  volatile bool m_flag;
+  volatile bool m_global_sense;
   volatile int m_counter;
-  volatile bool *m_local_sense;
+  bool *m_local_sense;
+  int *m_local_count;
   pthread_spinlock_t m_lock;
 };
 
 inline MyBarrier::MyBarrier(int participants)
 : m_participants(participants),
-  m_flag(false),
+  m_global_sense(false),
   m_counter(participants) {
   assert (participants > 0);
   m_local_sense = new (std::nothrow) bool[m_participants];
   assert (m_local_sense != NULL);
+  m_local_count = new (std::nothrow) int[m_participants];
+  assert (m_local_count != NULL);
   for (int i = 0; i < m_participants; ++i) {
     m_local_sense[i] = false;
+    m_local_count[i] = 0;
   }
-  int pshared = 0;
+  int pshared = PTHREAD_PROCESS_PRIVATE;
   int ret = pthread_spin_init(&m_lock, pshared); 
   assert (ret == 0);  
 }
 
 inline MyBarrier::~MyBarrier( ) {
   delete [] m_local_sense;
+  delete [] m_local_count;
   pthread_spin_destroy(&m_lock);
+  //check that barrier is no longer active - extra checking
+  assert (m_counter == m_participants);
 }
 
 inline int MyBarrier::Wait(int tid) {
@@ -48,18 +55,16 @@ inline int MyBarrier::Wait(int tid) {
   m_local_sense[tid] = !m_local_sense[tid];
   int lock_ret = pthread_spin_lock(&m_lock);
   assert (lock_ret == 0);
-  --m_counter;
-  if (m_counter == 0) {
-    int unlock_ret = pthread_spin_unlock(&m_lock);
-    assert (unlock_ret == 0);
+  m_local_count[tid] = --m_counter; 
+  int unlock_ret = pthread_spin_unlock(&m_lock);
+  assert (unlock_ret == 0);
+  if (m_local_count[tid] == 0) {
     m_counter = m_participants;
-    m_flag = m_local_sense[tid];
+    m_global_sense = m_local_sense[tid];
     return PTHREAD_BARRIER_SERIAL_THREAD;
   } else {
-    int unlock_ret = pthread_spin_unlock(&m_lock);
-    assert (unlock_ret == 0);
     __parsec_local_end(); // keep the busy loop out of data collection or simulation
-    while (m_flag != m_local_sense[tid]) {
+    while (m_global_sense != m_local_sense[tid]) {
       asm volatile("rep; nop" ::: "memory");
       asm volatile("rep; nop" ::: "memory");
     }
